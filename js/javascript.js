f2102c6906539eeb3c00493770961e57e0e06a46
oskartyrala <oskartyrala@protonmail.com>
2023-02-19 23:21:07 +0100

    Refactor and clean up code
    Introducing bucket fill was interacting with all the other functions
    so the code kept evolving to accommodate the interactions. It needed
    some cleaning up afterwards. I also introduced other improvements
    while I was at it.

--- js/javascript.js
+++ js/javascript.js
@@ -1,150 +1,32 @@
 const grid = document.querySelector(".grid");
-const inputs = document.querySelectorAll(".top > input");
 const horizontal = document.querySelector(".horizontal");
 const vertical = document.querySelector(".vertical");
 const wipe = document.querySelector(".wipe");
-const picker = document.querySelector(".picker");
 const modes = document.querySelectorAll(".mode");
-const pickerContainer = document.querySelector("#pick");
-const rainbow = document.querySelector("#rainbow");
-const erase = document.querySelector("#erase");
-const shade = document.querySelector(".shade");
-const lighten = document.querySelector(".lighten");
-const darken = document.querySelector(".darken");
-const brushSmall = document.querySelector(".small");
-const brushLarge = document.querySelector(".large");
+const pickerContainer = document.querySelector("#standard");
+const picker = document.querySelector(".picker");
+const slider = document.querySelector(".slider");
+const lightenIcon = document.querySelector(".lighten");
+const darkenIcon = document.querySelector(".darken");
+const brushes = document.querySelectorAll("button.brush");
 const borderless = document.querySelector(".borderless");
-const bordersIcon = document.querySelector(".borderless > .icon");
-const bucket = document.querySelector("#bucket");
-const bucketIcon = document.querySelector("#bucket > .icon")
 
-// Determines the current mode and "brush" color
+let pixels = [];
+let currentMode;
 let currentColor;
 let shadeKeeper;
 let brushSize = "small";
-let borders = true;
-let pixels = [];
+let pxBorders = true;
+
+// Set up the initial state
+setMode("standard");
 pickColor();
-markSelected();
-
-// Changes brush size between 1x1 and 3x3
-brushSmall.addEventListener("click", () => {
-    brushSize = "small";
-    brushSmall.classList.add("active");
-    brushLarge.classList.remove("active");
-    bucketIcon.src = "./img/bucket.svg";
-})
-
-brushLarge.addEventListener("click", () => {
-    brushSize = "large";
-    brushLarge.classList.add("active");
-    brushSmall.classList.remove("active");
-    bucketIcon.src = "./img/bucket.svg";
-})
-
-// Toggles pixel borders
-borderless.addEventListener("click", () => {
-    for (pixel of pixels) {
-        pixel.classList.toggle("noborders");
-    }
-
-    if (borders) {
-        bordersIcon.src = "./img/borders-on.svg";
-        borders = false;
-    } else {
-        bordersIcon.src = "./img/borders-off.svg";
-        borders = true;
-    }
-})
-
-// Activates the bucket
-bucket.addEventListener("click", () => {
-    brushSize = "bucket";
-        bucketIcon.src = "./img/bucket-active.svg";
-        brushLarge.classList.remove("active");
-        brushSmall.classList.remove("active");
-})
-
-// Changes mode to standard and updates the current "brush" color
-picker.addEventListener("input", pickColor);
-picker.addEventListener("click", pickColor);
-
-function pickColor() {
-    currentMode = "pick";
-    pickerContainer.style.backgroundColor = picker.value;
-    markSelected();
-    toggleShade();
-}
-
-function convertToRgb(hex) {
-    const hexArray = [hex.slice(-6, -4), hex.slice(-4, -2), hex.slice(-2)];
-
-    const rgbArray = [
-        parseInt(hexArray[0], 16),
-        parseInt(hexArray[1], 16),
-        parseInt(hexArray[2], 16),
-        1
-    ];
-
-    rgbString = `rgba(${rgbArray.join(", ")})`;
-    return rgbString;
-}
-
-// Changes mode to erase and changes the color to white;
-erase.addEventListener("click", () => {
-    currentMode = "erase";
-    markSelected();
-    toggleShade();
-})
-
-// Changes mode to rainbow
-rainbow.addEventListener("click", () => {
-    currentMode = "rainbow";
-    markSelected();
-    toggleShade();
-})
-
-// Adds a border around the current mode
-function markSelected() {
-    for (mode of modes) {
-        mode.classList.remove("selected");
-    }
-    document.getElementById(`${currentMode}`).classList.add("selected");
-}
-
-// Disables the shade slider when not in pick mode, then switches back to
-// last value when back in pick mode.
-shade.addEventListener("input", () => {
-    shadeKeeper = shade.value;
-    if (shade.value === "0") {
-        currentColor = convertToRgb(picker.value);
-    }
-})
-
-function toggleShade() {
-    if (currentMode === "pick") {
-        shade.style.pointerEvents = "";
-        shade.classList.remove("disabled");
-        shade.value = shadeKeeper;
-        darken.src = "./img/darken-enabled.svg"
-        lighten.src = "./img/lighten-enabled.svg"
-    } else {
-        shade.value = "0";
-        shade.classList.add("disabled");
-        shade.style.pointerEvents = "none";
-        darken.src = "./img/darken-disabled.svg"
-        lighten.src = "./img/lighten-disabled.svg"
-    }
-}
-
-// Creates the grid based on input
 createGrid(horizontal, vertical);
 
-for (input of inputs) {
-    input.addEventListener("input", () => {
-        createGrid(horizontal, vertical);
-    })
-}
+// Create grid based on user input. Limit grid size to between 1x1 and 100x100.
+// Add listeners to pixels to enable painting.
+horizontal.addEventListener("input", () => createGrid(horizontal, vertical));
+vertical.addEventListener("input", () => createGrid(horizontal, vertical));
 
 function createGrid(horizontal, vertical) {
     pixels = [];
@@ -168,25 +50,24 @@ function createGrid(horizontal, vertical) {
     for (let i = 0; i < (hValue * vValue); i++) {
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        if (!borders) {
+        if (!pxBorders) {
             pixel.classList.add("borderless");
         }
-        pixel.style.width = (100 / hValue) + "%";
+        
+        pixel.style.width = (600 / hValue) + "px";
         pixel.style.height = (600 / vValue) + "px";
 
         pixel.addEventListener("mousedown", (e) => {
             if (e.buttons === 1) {
                 e.preventDefault();
-                // updateColor(e);
-                paintPixels(e);
+                paintPixels(pixels.indexOf(e.target));
             }
         })
 
         pixel.addEventListener("mouseenter", (e) => {
             if (e.buttons === 1) {
                 e.preventDefault();
-                // updateColor(e);
-                paintPixels(e);
+                paintPixels(pixels.indexOf(e.target));
             }
         })
         
@@ -195,163 +76,262 @@ function createGrid(horizontal, vertical) {
     }
 }
 
-function paintPixels(e) {
+// Wipe the board cleanpixels.indexOf(e.target)
+wipe.addEventListener("click", () => createGrid(horizontal, vertical));
 
-    // Update color
+// Change the current mode
+for (mode of modes) {
+    mode.addEventListener("click", (e) => {
+        setMode(e.currentTarget.id);
+    })
+}
 
-    currentColor = convertToRgb(picker.value);
+function setMode(newMode) {
+    currentMode = newMode;
+    markSelected(newMode);
+    toggleShade();
+}
 
-    if (currentMode === "rainbow") {
-        currentColor = `rgb(${getRGBValue()}, ${getRGBValue()}, 
-        ${getRGBValue()})`;
+// Place a border around the selected mode
+function markSelected(newMode) {
+    for (mode of modes) {
+        mode.classList.remove("selected");
     }
+    document.getElementById(newMode).classList.add("selected");
+}
 
-    if (currentMode === "erase") {
-        currentColor = "rgba(255, 255, 255, 0)";
+// In erase and random mode, lock the shading toggle. They don't currently
+// interact well together. When back in standard mode, set toggle to the 
+// previous value.
+slider.addEventListener("input", () => {
+    shadeKeeper = slider.value;
+    if (slider.value === "0") {
+        currentColor = getRgbaFromHex(picker.value);
     }
+})
 
-    // Brush size logic
+function toggleShade() {
+    if (currentMode === "standard") {
+        slider.style.pointerEvents = "";
+        slider.classList.remove("disabled");
+        slider.value = shadeKeeper;
+        darkenIcon.src = "./img/darken-enabled.svg";
+        lightenIcon.src = "./img/lighten-enabled.svg";
+
+    } else {
+        slider.value = "0";
+        slider.classList.add("disabled");
+        slider.style.pointerEvents = "none";
+        darkenIcon.src = "./img/darken-disabled.svg";
+        lightenIcon.src = "./img/lighten-disabled.svg";
+    }
+}
+
+// Use the picker to select a color and reflect the choice on the button.
+picker.addEventListener("input", pickColor);
+picker.addEventListener("click", pickColor);
+
+function pickColor() {
+    pickerContainer.style.backgroundColor = picker.value;
+}
+
+// Select the brush size: small, large, or bucket.
+for (brush of brushes) {
+    brush.addEventListener("click", (e) => {
+        brushSize = e.currentTarget.id;
+        for (brush of brushes) {
+            brush.classList.remove("active");
+        };
+        e.currentTarget.classList.add("active");
+    })
+}
+
+// Toggle pixel borders.
+borderless.addEventListener("click", () => {
+    for (pixel of pixels) {
+        pixel.classList.toggle("noborders");
+    }
+    borderless.classList.toggle("active");
+})
+
+// Paint the pixels based on brush size.
+function paintPixels(pxIndex) {
+
     if (brushSize === "bucket") {
-        paintBucket(e.target);
+        paintBucket(pxIndex);
     }
 
     if (brushSize === "large") {
-        paintLargeBrush(e.target);
+        paintLargeBrush(pxIndex);
     }
 
     if (brushSize === "small") {
-        paintSinglePixel(pixels.indexOf(e.target));
+        paintSmallBrush(pxIndex);
     }
 }
 
+// Paints a single pixel if it exists. Update the color beforehand based on the
+// current mode and shading toggle value.
+function paintSmallBrush(pxIndex) {
 
-
-
-
-function paintBucket(pixel) {
-    const bucketTargetColor = getRgbaFromRgb(pixel.style.backgroundColor);
-    const pxIndex = pixels.indexOf(pixel);
-
-        paintAround(pxIndex, bucketTargetColor);
-}
-
-function paintAround(pxIndex, bucketTargetColor) {
-
-    // Attempts to paint around the bucket target if the color matches.
-    if (getRgbaFromRgb(pixels[pxIndex].style.backgroundColor) === bucketTargetColor) {
-        paintSinglePixel(pxIndex);
-        if (bucketTargetColor !== currentColor) {
-        // Paints pixels on the sides if they don't go over the edge
-        if (pxIndex !== 0 && 
-            pxIndex % hValue > (pxIndex - 1) % hValue) {
-            paintAround(pxIndex - 1, bucketTargetColor);
-        }
-    
-        if (pxIndex % hValue < (pxIndex + 1) % hValue) {
-            paintAround(pxIndex + 1, bucketTargetColor);
-        }
-    
-        // Paints pixels on top and bottom if they don't go over the edge
-        if (pxIndex - hValue > 0) {
-            paintAround(pxIndex - hValue, bucketTargetColor);
-        }
-    
-        if (pxIndex + hValue < hValue * vValue) {
-            paintAround(pxIndex + hValue, bucketTargetColor);
-        }
-        }
-
-    }
-}
-
-// Gets a random number to feed into random color in rainbow mode
-function getRGBValue() {
-    return Math.floor(Math.random() * 255) + 1;
-}
-
-// Increases or decreases current color's alpha by 0.1
-function shadePixel(pixel) {
-
-    currentColor = convertToRgb(picker.value);
-
-    const pixelColorString = getRgbaFromRgb(pixel.style.backgroundColor);
-    let pixelColorArray = pixelColorString.split(", ");
-    let alphaValue = Number(pixelColorArray[3].slice(0, -1));
-
-    // Depending on the shade toggle, adjusts the alpha value
-    if (shade.value === "-1" && alphaValue < 1) {
-        alphaValue += 0.1;
-        pixelColorArray[3] = alphaValue + ")";
-    } else if (shade.value === "1" && alphaValue > 0) {
-        alphaValue -= 0.1;
-        pixelColorArray[3] = alphaValue + ")";
-    }
-
-    if (!alphaValue) {
-        currentColor = "rgba(255, 255, 255, 0)";
-    } else {
-        const currentColorArray = currentColor.split(", ");
-        currentColorArray[3] = pixelColorArray[3];
-        currentColor = currentColorArray.join(", ");
-    }
-
-    // Combines the new alpha value with the current color and paints the pixel
-}
-
-// Paints a single pixel if it exists
-function paintSinglePixel(pxIndex) {
-
-    if (shade.value !== "0") {
-        shadePixel(pixels[pxIndex]);
-    }
     if (pxIndex >= 0 &&
         pxIndex <= hValue * vValue) {
+
+        updateColor(pxIndex);
 
         pixels[pxIndex].style.backgroundColor = currentColor;
     }
 }
 
-// Paints around the selected pixel while respecting edges and corners
-function paintLargeBrush(pixel) {
-    let pxIndex = pixels.indexOf(pixel);
+function updateColor(pxIndex) {
 
-    paintSinglePixel(pxIndex);
+    if (currentMode === "standard") {
+        currentColor = getRgbaFromHex(picker.value);
 
-    // Paints pixels on top and bottom if they don't go over the edge
-    paintSinglePixel(pxIndex - hValue);
-    paintSinglePixel(pxIndex + hValue);
-
-        // Paints pixels on the sides if they don't go over the edge
-    if (pxIndex !== 0 &&
-        pxIndex % hValue > (pxIndex - 1) % hValue) {
-        paintSinglePixel(pxIndex - 1 - hValue);
-        paintSinglePixel(pxIndex - 1);
-        paintSinglePixel(pxIndex - 1 + hValue);
+    } else if (currentMode === "random") {
+        currentColor = `rgb(${getRandomRgbValue()}, ${getRandomRgbValue()}, 
+        ${getRandomRgbValue()})`;
+        
+    } else if (currentMode === "erase") {
+        currentColor = "rgba(255, 255, 255, 0)";
     }
-    if (pxIndex % hValue < (pxIndex + 1) % hValue) {
-        paintSinglePixel(pxIndex + 1 - hValue);
-        paintSinglePixel(pxIndex + 1);
-        paintSinglePixel(pxIndex + 1 + hValue);
+
+    if (slider.value !== "0") {
+        shadeColor(pixels[pxIndex]);
     }
 }
 
-function getRgbaFromRgb(rgbString) {
-    // If the pixel hasn't been colored yet, its backgroundColor will return empty. In that case, we assume fully transparent white.
-    if (!rgbString) {
-        rgbString = "rgba(255, 255, 255, 0)";
-    };
+// Get a random number to feed into currentColor in random mode.
+function getRandomRgbValue() {
+    return Math.floor(Math.random() * 255) + 1;
+}
 
-    const rgbArray = rgbString.split(", ");
-    if (!rgbArray[3]) {
-        rgbArray[2] = rgbArray[2].slice(0, -1);
-        rgbArray[3] = "1)";
-    };
-    rgbArray[0] = rgbArray[0].replace("rgb(", "rgba(");
-    rgbaString = rgbArray.join(", ");
+// Converts hex to rgba - REFACTORED
+function getRgbaFromHex(hexString) {
+    const hexArray = [hexString.slice(-6, -4), hexString.slice(-4, -2), hexString.slice(-2)];
+
+    const rgbaArray = [
+        parseInt(hexArray[0], 16),
+        parseInt(hexArray[1], 16),
+        parseInt(hexArray[2], 16),
+        1
+    ];
+
+    rgbaString = `rgba(${rgbaArray.join(", ")})`;
     return rgbaString;
 }
 
-// Wipes the board
-wipe.addEventListener("click", () => {
-    createGrid(horizontal, vertical);
-})
+// Transform rgb colors to rgb. This ensures all modes properly recognize
+// a pixel's color and compare it to the current color or other pixel colors.
+function getRgbaFromRgb(rgbString) {
+    // If the pixel hasn't been colored yet, its backgroundColor will return 
+    // empty. In that case, we assume fully transparent white in order to work
+    // well with all modes.
+    if (!rgbString) {
+        rgbString = "rgba(255, 255, 255, 0)";
+    }
+
+    const rgbArray = rgbString.split(", ");
+    rgbaArray = rgbArray;
+    if (!rgbaArray[3]) {
+        rgbaArray[2] = rgbaArray[2].slice(0, -1);
+        rgbaArray[3] = "1)";
+    }
+    rgbaArray[0] = rgbaArray[0].replace("rgb(", "rgba(");
+    rgbaString = rgbaArray.join(", ");
+    return rgbaString;
+}
+
+// Increase or decrease current color's alpha value by 0.1 based on the painted
+// pixel's current alpha value.
+function shadeColor(pixel) {
+    currentColor = getRgbaFromHex(picker.value);
+
+    const pxColorString = getRgbaFromRgb(pixel.style.backgroundColor);
+    let pxColorArray = pxColorString.split(", ");
+    let alphaValue = Number(pxColorArray[3].slice(0, -1));
+
+    if (slider.value === "-1" && alphaValue < 1) {
+        alphaValue += 0.1;
+        pxColorArray[3] = alphaValue + ")";
+    } else if (slider.value === "1" && alphaValue > 0) {
+        alphaValue -= 0.1;
+        pxColorArray[3] = alphaValue + ")";
+    }
+
+    // If the pixel is fully transparent, assume white in order to work well 
+    // with the bucket's color recognition logic.
+    if (!alphaValue) {
+        currentColor = "rgba(255, 255, 255, 0)";
+    } else {
+        const currentColorArray = currentColor.split(", ");
+        currentColorArray[3] = pxColorArray[3];
+        currentColor = currentColorArray.join(", ");
+    }
+}
+
+// Paint around the selected pixel while respecting edges and corners.
+function paintLargeBrush(pxIndex) {
+
+    paintSmallBrush(pxIndex);
+
+    // Paint pixels on top and bottom if they don't go over the edge
+    // (top and bottom edge logic inside paintSmallBrush).
+    paintSmallBrush(pxIndex - hValue);
+    paintSmallBrush(pxIndex + hValue);
+
+    // Paint pixels on the sides if they don't go over the edge.
+    if (pxIndex !== 0 &&
+        pxIndex % hValue > (pxIndex - 1) % hValue) {
+        paintSmallBrush(pxIndex - 1 - hValue);
+        paintSmallBrush(pxIndex - 1);
+        paintSmallBrush(pxIndex - 1 + hValue);
+    }
+    if (pxIndex % hValue < (pxIndex + 1) % hValue) {
+        paintSmallBrush(pxIndex + 1 - hValue);
+        paintSmallBrush(pxIndex + 1);
+        paintSmallBrush(pxIndex + 1 + hValue);
+    }
+}
+
+// Color pixels around the target pixel as long as they have the same color.
+// Coloring stops when it encounters grid edge or a different colored pixel.
+function paintBucket(pxIndex) {
+    const bucketTargetColor = getRgbaFromRgb(pixels[pxIndex].style.backgroundColor);
+    paintTillBorder(pxIndex, bucketTargetColor);
+}
+
+function paintTillBorder(pxIndex, bucketTargetColor) {
+
+    const adjacentPxColor = getRgbaFromRgb(pixels[pxIndex].style.backgroundColor);
+
+    // Attempt to paint adjacent pixels as long as the color matches with
+    // the target pixel.
+    if (adjacentPxColor === bucketTargetColor) {
+        
+        paintSmallBrush(pxIndex);
+
+        // Paint adjacent pixels until it encounters a pixel of the same color.
+        if (bucketTargetColor !== currentColor) {
+
+            // Paint pixels on the sides if they don't go over the edge.
+            if (pxIndex !== 0 && 
+                pxIndex % hValue > (pxIndex - 1) % hValue) {
+                paintTillBorder(pxIndex - 1, bucketTargetColor);
+            }
+        
+            if (pxIndex % hValue < (pxIndex + 1) % hValue) {
+                paintTillBorder(pxIndex + 1, bucketTargetColor);
+            }
+        
+            // Paint pixels on top and bottom if they don't go over the edge.
+            if (pxIndex - hValue > 0) {
+                paintTillBorder(pxIndex - hValue, bucketTargetColor);
+            }
+        
+            if (pxIndex + hValue < hValue * vValue) {
+                paintTillBorder(pxIndex + hValue, bucketTargetColor);
+            }
+        }
+    }
+}
